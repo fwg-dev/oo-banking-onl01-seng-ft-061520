5643c6ad3c4045ae5caa49c8c0b2411ed2a2a3ea
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-01 18:02:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
   # your code here
+
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
+  def initialize(sender, receiver, amount)
+
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+
+    # @reciver = reciver
+ end
+ def valid?
+   sender.valid? && self.valid?
+end
 end
