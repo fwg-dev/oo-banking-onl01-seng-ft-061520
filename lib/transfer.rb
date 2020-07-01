375a202a87e593838c3dce75f5bb2fbce8bb866d
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-01 18:35:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ end
 def execute_transaction
   if valid? && sender.balance > amount && self.status == "pending"
     sender.balance -= amount
-    reciever.balance += amount
+    receiver.balance += amount
     self.status = "complete"
   else
     reject_transfer
