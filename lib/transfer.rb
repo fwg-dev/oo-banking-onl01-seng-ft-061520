b671760fb3f264da14ecb232e88939a03c4f2268
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-01 18:32:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,33 +10,29 @@ class Transfer
     @status = "pending"
     @amount = amount
 
-    # @reciver = reciver
  end
  def valid?
    sender.valid? && self.valid?
 end
 def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      self.status = "complete"
-    else
-      reject_transfer
-    end
-  end
-
-  def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == “complete”
-      receiver.balance -= amount
-      sender.balance += amount
-      self.status = “reversed”
-    else
-      reject_transfer
-    end
-  end
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
-
-end
+  if valid? && sender.balance > amount && self.status == "pending"
+    sender.balance -= amount 
+    reciever.balance = += amount 
+    self.status = "complete"
+  else 
+    reject_transfer
+  end 
+end 
+ def reverse_transfer
+     if valid? && sender.balance > amount && self.status == "complete" 
+       reciever.balance = -= amount 
+       sender.balance += amount
+       self.status = "reversed"
+     else 
+       reject_transfer
+     end
+end 
+def reject_transfer
+  self.status = "rejected"
+  "Transaction rejected. Please checl your account balance."
+end 
