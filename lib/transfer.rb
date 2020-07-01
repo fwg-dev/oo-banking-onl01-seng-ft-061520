4eb02b37fcc7b203c330ecb73af0470c76c0d5da
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-01 18:35:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,6 +34,6 @@ end
 end
 def reject_transfer
   self.status = "rejected"
-  "Transaction rejected. Please checl your account balance."
+  "Transaction rejected. Please check your account balance."
 end
 end
